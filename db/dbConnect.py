bff0598469ced43831b711b31a132cd80047c8ec
Kleber Alcanfor <kalcanfor@cgee.org.br>
2023-07-28 15:03:09 -0300

    teste de conexão com o banco

---
+++ db/dbConnect.py
@@ -0,0 +1,23 @@
+import sqlite3
+import pandas as pd
+from fastapi import FastAPI
+
+class Backend:
+
+    def __init__(self):
+        self.conn = sqlite3.connect('db/dadosESP8266.db')
+        self.checkDB()
+
+    def checkDB(self):
+        tables = pd.read_sql_query("SELECT name FROM sqlite_schema WHERE type ='table' AND name = 'monitor'", self.conn)
+        if(len(tables)==0):
+            sqlCreateMonitor = '''
+                CREATE TABLE monitor
+                        (id char(32) PRIMARY KEY NOT NULL,
+                        fonte CHAR(20),
+                        data_status_atual   char(20),
+                        temperatura NUMERIC,
+                        umidade NUMERIC,
+                        situacao CHAR(1));
+                '''
+            self.conn.execute(sqlCreateMonitor)
---
+++ test.py
@@ -0,0 +1,6 @@
+import sys
+
+sys.path.append("db")
+from dbConnect  import Backend 
+
+db = Backend()
