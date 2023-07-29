9fc088f6b5b36c1522b7b7ef9cabe136e3b0858c
Vinicius Alcanfor <vinicius.alcanfor@gmail.com>
2023-07-29 18:03:33 -0300

    Inclusão dos sensores e Att dos parâmetros

--- test.py
+++ test.py
@@ -1,6 +1,15 @@
-import sys
+import requests
 
-sys.path.append("db")
-from dbConnect  import Backend 
+reg = {
+    "id": "002",
+    "fonte": "esp1",
+    "data_status_atual": "28/07/2023 16:20",
+    "temperatura": 23.5,    
+    "umidade": 79.0,
+    "situacao": "ok"
+}
 
-db = Backend()
+headers = {}
+
+response = requests.post("http://192.168.15.10:8000/monitor", json=reg)
+print(response)
